9183ce2df28886242902c720e6bd87022092d144
Puspita Kartika Sari <91021761+PuspitaKartika@users.noreply.github.com>
2023-10-13 12:38:12 +0700

    Create model class task

---
+++ lib/model/task_model.dart
@@ -0,0 +1,15 @@
+class TaskModel {
+  int? id;
+  late String taskName;
+
+  TaskModel({this.id, required this.taskName});
+
+  Map<String, dynamic> toMap() {
+    return {'id': id, 'taskName': taskName};
+  }
+
+  TaskModel.fromMap(Map<String, dynamic> map) {
+    id = map['id'];
+    taskName = map['taskName'];
+  }
+}
