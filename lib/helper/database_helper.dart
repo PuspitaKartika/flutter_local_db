06318944e221d47534f492ce2137599089dc9aab
Puspita Kartika Sari <91021761+PuspitaKartika@users.noreply.github.com>
2023-10-13 12:42:22 +0700

    create DatabaseHelper and add factory constructor

---
+++ lib/helper/database_helper.dart
@@ -0,0 +1,9 @@
+class DatabaseHelper {
+  static DatabaseHelper? _databaseHelper;
+
+  DatabaseHelper._internal() {
+    _databaseHelper = this;
+  }
+
+  factory DatabaseHelper() => _databaseHelper ?? DatabaseHelper._internal();
+}
