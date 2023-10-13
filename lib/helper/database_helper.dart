ab58e242bb8e31b4883ac3b9b80f2cb63af5a386
Puspita Kartika Sari <91021761+PuspitaKartika@users.noreply.github.com>
2023-10-13 13:06:27 +0700

    membuka koneksi ke database dan membuat tabel

--- lib/helper/database_helper.dart
+++ lib/helper/database_helper.dart
@@ -1,9 +1,35 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
 class DatabaseHelper {
   static DatabaseHelper? _databaseHelper;
+  static late Database _database;
 
   DatabaseHelper._internal() {
     _databaseHelper = this;
   }
 
   factory DatabaseHelper() => _databaseHelper ?? DatabaseHelper._internal();
+
+  // Membuat objek database
+  Future<Database> get database async {
+    _database = await _initializeDb();
+    return _database;
+  }
+
+  //membuat koneksi database dan membuat tabel
+  final String _tabelName = 'tasks';
+
+  Future<Database> _initializeDb() async {
+    var db = openDatabase(join(await getDatabasesPath(), 'task_db.db'),
+        onCreate: (db, version) async {
+      await db.execute(
+        '''CREATE TABLE $_tabelName(
+            id INTEGER PRIMARY KEY,
+            taskName TEXT
+          )''',
+      );
+    }, version: 1);
+    return db;
+  }
 }
